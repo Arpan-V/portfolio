2fb8fcf0f78c265b1d53026830aeb844c7b0648c
Arpan Verma <131739133+Arpan-V@users.noreply.github.com>
2026-08-01 13:16:34 +0000

    add vercel url in robots.ts and sitemap.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,14 @@
-// import type { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
-// export default function robots(): MetadataRoute.Robots {
-//   const baseUrl = "https://yourdomain.com";
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://arpanverma.vercel.app/";
 
-//   return {
-//     rules: {
-//       userAgent: "*",
-//       allow: "/",
-//     },
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
 
-//     sitemap: `${baseUrl}/sitemap.xml`,
-//   };
-// }
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,20 +1,20 @@
-// import type { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   const baseUrl = "https://yourdomain.com";
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://arpanverma.vercel.app/";
 
-//   return [
-//     {
-//       url: baseUrl,
-//       lastModified: new Date(),
-//       changeFrequency: "monthly",
-//       priority: 1,
-//     },
-//     {
-//       url: `${baseUrl}/projects/lens`,
-//       lastModified: new Date(),
-//       changeFrequency: "monthly",
-//       priority: 0.8,
-//     },
-//   ];
-// }
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/projects/lens`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
